af64988f7bf15c4878ab83c6887f5029f5a0c1b0
jardelmods11-beep <jardelmods11@gmail.com>
2026-02-07 08:42:27 -0300

    Update api_server.py

--- api_server.py
+++ api_server.py
@@ -7,7 +7,7 @@ import os
 app = Flask(__name__)
 
 # Token de acesso (pode vir de variável de ambiente)
-TOKEN = os.environ.get('TOKEN', 'LRZOJID1')
+TOKEN = os.environ.get('TOKEN', '2G4B7UIZ')
 
 # Inicializa o scraper globalmente
 scraper = None
